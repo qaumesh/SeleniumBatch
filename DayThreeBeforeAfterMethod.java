a81efb7404ef7fad766e973989d8f5b7618c3d41
qaumesh <105368797+qaumesh@users.noreply.github.com>
2024-08-29 09:40:16 +0530

    Create DayThreeBeforeAfterMethod.java

---
+++ DayThreeBeforeAfterMethod.java
@@ -0,0 +1,63 @@
+package com.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DayThreeBeforeAfterMethod {
+
+	WebDriver driver=null;
+	
+	@BeforeMethod
+	
+	public void InitialiseBrowser() {
+	driver = new ChromeDriver();
+	System.out.println("Initialise browser");
+	driver.get("https://www.saucedemo.com");
+	System.out.println("open  website");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	
+	@Test
+	public void verifyPageTitle() {
+	System.out.println("verifying page title");
+	String actualPageTitle=driver.getTitle();
+	System.out.println("Actual page title :"+actualPageTitle);
+	Assert.assertEquals(actualPageTitle, "Swag Labs");
+	}
+	
+	@Test
+	public void validateLogicbuttonText() {
+		System.out.println("validateLogicbuttonText");
+		WebElement loginButton= driver.findElement(By.id("login-button"));
+		String loginButtonText=loginButton.getAttribute("value");
+		Assert.assertEquals(loginButtonText, "Login");
+	}
+	
+	
+	
+	@Test
+	public void verifyLogicButtonDisplay() {
+		System.out.println("verifyLogicButtonDisplay");
+		WebElement loginButton= driver.findElement(By.id("login-button"));
+		boolean flag=loginButton.isDisplayed();
+		Assert.assertTrue(flag);
+	}
+	
+	
+	
+	@AfterMethod
+	public void stopCar() {
+		driver.close();
+	}
+	
+}
