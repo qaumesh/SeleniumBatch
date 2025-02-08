159e868161e6da24af03105a7bfb76dc8691f6fd
qaumesh <105368797+qaumesh@users.noreply.github.com>
2025-02-08 11:15:10 +0530

    Update DayOne.java

--- DayOne.java
+++ DayOne.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DayOne {
-
 	public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver();
